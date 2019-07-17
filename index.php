b01d876384f35679ed3192933bf8b09b0629a9a1
npal4183 <42148500+npal4183@users.noreply.github.com>
2019-07-17 14:51:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,27 @@
 </head>
 <body>
   <h1>Create User Profile</h1>
+  
+  <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.3.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCU-pl0EApTzv1gDQyw9eJ9mrYr2cg-vPo",
+    authDomain: "create-user-profile.firebaseapp.com",
+    databaseURL: "https://create-user-profile.firebaseio.com",
+    projectId: "create-user-profile",
+    storageBucket: "",
+    messagingSenderId: "850996018185",
+    appId: "1:850996018185:web:bb4169e42246d3ed"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
+  
 </body>
 </html>
